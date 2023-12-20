b94211668a94b40bc9dfa8b5012d44d053bbb24c
gsspdev <dev@gssp.io>
2023-12-20 01:58:19 -0800

    updated cpal initialization

--- src/audio_host.rs
+++ src/audio_host.rs
@@ -1,35 +1,36 @@
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-
-pub fn initialize_audio_stream() {
-    let output_stream = prepare_audio_stream();
-
-    let output_stream_play = output_stream.play();
-    // other implementation...
-}
-
-fn prepare_audio_stream() -> cpal::Stream {
-    let host = cpal::default_host();
-    let device = host.default_output_device().expect("no output device available");
-    let mut supported_configs_range = device.supported_output_configs()
-        .expect("error while querying configs");
-    let supported_config = supported_configs_range.next()
-        .expect("no supported config?!")
-        .with_max_sample_rate();
-    let config = supported_config.config();
-
-    let output_stream = device.build_output_stream(
-        &config,
-        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-            // react to stream events and read or write stream data here.
-        },
-        move |err| {
-            // react to errors here.
-        },
-        None, // None=blocking, Some(Duration)=timeout
-    ).unwrap();
-
-    output_stream
-// pub fn play_audio() {
-//     stream.play().unwrap();
+// use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 //
-}
+// pub fn initialize_audio_stream() {
+//     let output_stream = prepare_audio_stream();
+//
+//     let output_stream_play = output_stream.play().unwrap();
+//     // other implementation...
+// }
+//
+// fn prepare_audio_stream() -> cpal::Stream {
+//     let host = cpal::default_host();
+//     let device = host.default_output_device().expect("no output device available");
+//     let mut supported_configs_range = device.supported_output_configs()
+//         .expect("error while querying configs");
+//     let supported_config = supported_configs_range.next()
+//         .expect("no supported config?!")
+//         .with_max_sample_rate();
+//     let config = supported_config.config();
+//
+//     let output_stream = device.build_output_stream(
+//         &config,
+//         move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+//             // react to stream events and read or write stream data here.
+//         },
+//         move |err| {
+//             // react to errors here.
+//         },
+//         None, // None=blocking, Some(Duration)=timeout
+//     ).unwrap();
+//
+//     output_stream
+// // pub fn play_audio() {
+// //     stream.play().unwrap();
+// //
+// }
+
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,73 @@
-mod audio_host;
-// mod available_hosts;
-use cpal::*;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use std::time::{Duration, Instant};
+use cpal::SampleRate;
+use cpal::{Device, Host, StreamConfig};
+
+pub struct AudioEnvironment {
+    host: Host,
+    device: Device,
+    config: StreamConfig,
+    sample_rate: SampleRate,
+}
+
+
+impl AudioEnvironment {
+    fn audio_prepare() -> Self {
+        let host: Host = cpal::default_host();
+        let device: Device = host
+            .default_output_device()
+            .expect("Failed to find default output device.");
+        let config: StreamConfig = device
+            .default_output_config()
+            .expect("Failed to get default output config")
+            .config();
+        let sample_rate: SampleRate = config.sample_rate;
+        return AudioEnvironment {
+            host,
+            device,
+            config,
+            sample_rate,
+        };
+    }
+}
+
+pub fn run() {
+    let audio_env = AudioEnvironment::audio_prepare();
+    const update_interval: Duration = Duration::from_secs(60);
+    let mut time: u32 = 0;
+
+    let sample_rate = 44100 as f32;
+    let mut samples_played = 0f32;
+
+    let err_fn = |err| eprintln!("An error occurred on the output audio stream: {}", err);
+
+    let stream = audio_env.device
+        .build_output_stream(
+            &audio_env.config,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                for sample in data.iter_mut() {
+                    let time = samples_played / sample_rate;
+                    *sample = time.sin();
+                    samples_played += 1.0;
+                }
+            },
+            err_fn, Some(Duration::from_secs(60))
+        )
+        .expect("Failed to build output stream.");
+
+    stream.play().expect("Failed to start audio stream.");
+
+    println!("Press Enter to stop the audio.");
+    let _ = std::io::stdin().read_line(&mut String::new());
+
+    loop {
+        let start = Instant::now();
+        let mut output = 0;
+        println!("Output at time {}: {}", time, output);
+    }
+}
 
 fn main() {
-    // Assuming the module is named `audio_host`
-    // let audio_playing = audio_host::initialize_audio_stream();
-    audio_host::initialize_audio_stream();
-    dbg!(cpal::available_hosts());
+    run();
 
 }
